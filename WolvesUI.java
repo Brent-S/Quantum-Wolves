614dd7144af86736012bb45a99858580524d4b92
Jamie <jamie@violet>
2013-06-08 19:13:46 +0100

    added stuff

--- WolvesUI.java
+++ WolvesUI.java
@@ -1,4 +1,25 @@
 
 public interface WolvesUI {
+	
+	public static byte VISION_INNO = 1;
+	public static byte VISION_WOLF = 2;
+	public static byte VISION_NOPE = 0;
+	
+	public static int INDEX_GOOD = 0;
+	public static int INDEX_EVIL = 1;
+	public static int INDEX_ALIVE = 2;
+	public static int INDEX_DEAD = 3;
+	
+	
+	public int getNumPlayers();
+	public int getNumWolves();
+	
+	public int[] inputWolfTargets();
+	public int[] inputSeerTargets();
+	public void displayVisions(byte[] visions);
+	public int inputLynchTarget();
+	public void displayProbabilities();
+	
+	
 
 }
