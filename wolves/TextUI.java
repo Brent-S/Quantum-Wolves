a4e26ccd009f64f3bbad619d94447993bedbbd0e
Jamie <jamie@violet>
2013-06-08 23:04:11 +0100

    finished ui code

---
+++ wolves/TextUI.java
@@ -0,0 +1,48 @@
+package wolves;
+
+
+public class TextUI implements WolvesUI {
+
+	@Override
+	public int getNumPlayers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumWolves() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[] inputWolfTargets() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] inputSeerTargets() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void displayVisions(byte[] visions) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int inputLynchTarget() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void displayProbabilities(double[][] probabilities, int[] knownRoles) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ wolves/TextWolves.java
@@ -0,0 +1,149 @@
+package wolves;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class TextWolves implements WolvesUI {
+	
+	private int players;
+	private int wolves;
+	private int[] recentTargets;
+
+	@Override
+	public int getNumPlayers() {
+		while (true) {
+			try {
+				this.players = Integer.parseInt(getUserInput("PLEASE CHOOSE HOW MANY OF PEOPLE"));
+				return players;
+			} catch (Exception e) {
+				System.out.println("FUCK OFF THATS NOT A NUMBER");
+			}
+		}
+	}
+
+	@Override
+	public int getNumWolves() {
+		while (true) {
+			try {
+				this.wolves = Integer.parseInt(getUserInput("PLEASE CHOOSE HOW MANY OF WOLVES"));
+				return wolves;
+			} catch (Exception e) {
+				System.out.println("FUCK OFF THATS NOT A NUMBER");
+			}
+		}
+	}
+	
+	private String getUserInput(String prompt) {
+		BufferedReader c = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(prompt);
+				try {
+					return c.readLine();
+				} catch (IOException e) {
+					throw (new RuntimeException(e));
+				}
+	}
+
+	@Override
+	public int[] inputWolfTargets() {
+		int[] targets = new int[players];
+		for (int i = 0; i < players; i++) {
+			while (true) {
+				try {
+					targets[i] = Integer.parseInt(getUserInput("PLEASE CHOOSE WHO IS WOLVED BY PLAYER " + i));
+				} catch (Exception e) {
+					System.out.println("FUCK OFF THATS NOT A NUMBER");
+				}
+			}
+		}
+		return targets;
+	}
+
+	@Override
+	public int[] inputSeerTargets() {
+		int[] targets = new int[players];
+		for (int i = 0; i < players; i++) {
+			while (true) {
+				try {
+					targets[i] = Integer.parseInt(getUserInput("PLEASE CHOOSE WHO IS SEED BY PLAYER " + i));
+				} catch (Exception e) {
+					System.out.println("FUCK OFF THATS NOT A NUMBER");
+				}
+			}
+		}
+		this.recentTargets = targets;
+		return targets;
+	}
+
+	@Override
+	public void displayVisions(byte[] visions) {
+		for (int i = 0; i < players; i++) {
+			String role = null;
+			switch (visions[i]) {
+			case WolvesUI.VISION_INNO:
+				role = "VILLAGER";
+			case WolvesUI.VISION_WOLF:
+				role = "WHEREWOLF";
+			}
+			if (visions[i] != 0) {
+				System.out.println("PLAYER " + i + " SEES THAT PLAYER " + recentTargets[i] + " IS " + getAdjective() + " " + role);
+			}
+		}
+	}
+
+	private String getAdjective() {
+		String[] words = {"A FILTHY", "A CLEAN", "A CAPRICIOUS", "A WELL-INFORMED", "AN INSANE", "A SANITARY", "A TUMULTUOUS",
+				"AN INFLAMED", "AN INFLAMMATORY", "A CALAMATOUS", "AN INCONGRUOUS", "A RADISHY", "A FERAL", "A FINGER-LICKIN'", 
+				"A PERNICIOUS", "A LOOPY", "A DIRTY", "AN ANGRY", "A FOCUSED", "AN ANTICLIMATIC", "A HORRIBLE",
+				"A SCARY", "A MONSTROUS", "A TERRIBLE", "A TERRIFYING", "A DESPICABLE", "A SHIT",
+				"AN OVERPRICED", "AN EGREGIOUS", "A GREGARIOUS", "A MISSPELLED", "A HORRIFIC", "A FUCKED-UP",
+				"AN ABOMINABLE", "A COLD-HEARTED", "A HERETICAL", "A CANTANKEROUS", "A POLISHED", "A HAPPY",
+				"A FUZZY", "A FLUFFY", "A CUTE", "A TECHNOLOGICAL", "A MEGARIFFIC", "A RURAL", "A MORBID",
+				"A GRIM", "AN ANIMALISTIC", "A FRIENDLY", "A KIND-HEARTED", "A MURDEROUS", "A DANGEROUS",
+				"A BRUTAL", "A CALM", "A PSYCHOTIC", "A PSYCOPATHIC", "AN EVIL"};
+		Random generator = new Random();
+		return words[generator.nextInt(words.length)];
+	}
+
+	@Override
+	public int inputLynchTarget() {
+		while (true) {
+			try {
+				return Integer.parseInt(getUserInput("PLEASE CHOOSE WHO IS MADE OF LYNCHED"));
+			} catch (Exception e) {
+				System.out.println("FUCK OFF THATS NOT A NUMBER");
+			}
+		}
+	}
+
+	@Override
+	public void displayProbabilities(double[][] probabilities, int[] knownRoles) {
+		System.out.println("SORRY THIS ARENT ALIGNED");
+		System.out.println("PLAYER GOOD EVIL ALIVE DEAD");
+		for (int i = 0; i < players; i++) {
+			System.out.print(i + " ");
+			for (int j = 0; j < 4; i++) {
+				System.out.print(probabilities[i] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("KNOWN ROLLS");
+		for (int i = 0; i < players; i++) {
+			String role = null;
+			switch (knownRoles[i]) {
+			case ROLE_BLUE:
+				role = "VILLAGER";
+			case ROLE_SEER:
+				role = "SEER";
+			case ROLE_WOLF:
+				role = "WOLF";
+			}
+			if (knownRoles[i] != 0) {
+				System.out.println("PLAYER " + i + " IS " + getAdjective() + role);
+			}
+		}
+	}
+
+}
