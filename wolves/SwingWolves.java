7e78f15243a963f59b67ecd412707a5a9298025b
Brent-S <brentstewart3.1@googlemail.com>
2013-07-18 12:07:36 +0100

    Added SwingWolves UI

---
+++ wolves/SwingWolves.java
@@ -0,0 +1,89 @@
+package wolves;
+
+public class SwingWolves implements WolvesUI {
+
+	@Override
+	public int getNumPlayers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumWolves() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void displayEndGame(int RoundNum, WinCodes WinCode, int[] knownRoles) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int inputLynchTarget() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void displayProbabilities(double[][] probabilities, int[] knownRoles) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int inputSeerTarget(int inSeer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void displaySingleVision(int Seer, int Target, byte Vision) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void displayAllStates(String AllStateText) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean getDebugMode() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int InputSingleWolfTarget(int inPlayer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String inputName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void displayPlayerIDs(String[] inArray) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String[] SetNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void displayHistory(String HistoryText) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
